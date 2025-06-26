fb195271d8cd0bc9dbe929cde031e82d729f2e57
Mrudula-DiggiByte <mrudula.r@diggibyte.com>
2025-06-26 18:33:22 +0530

    Added

---
+++ Lists.py
@@ -0,0 +1,42 @@
+# marks=[94.4,87.5,95.2,66.4,45.1]
+# print(marks)
+# print(type(marks))
+# print(marks[0])
+# print(marks[1])
+# print(len(marks))
+
+# student =["mrudu",95.4,17,"Delhi"]
+# print(student)
+# student[0]="dee"
+# print(student)
+
+# mark=[85,94,76,63,48]
+# print(mark[1:4])
+# print(mark[1:])
+# print(mark[-3:-1])
+
+
+# list=[2,1,3]
+# list.append(4)
+# print(list)
+# print(list.sort())
+# print(list)
+# print(list.sort(reverse=True))
+# print(list)
+
+fruits=["banana","Litchi","apple"]
+fruits.sort()
+print(fruits)
+fruits.sort(reverse=False)
+print(fruits)
+fruits.reverse()
+print(fruits)
+fruits.insert(1,"mango")
+print(fruits) 
+fruits.remove("banana")
+print(fruits)
+fruits.pop(1)
+print(fruits)
+
+veggie=fruits.copy()
+print(veggie.count("banana"))
